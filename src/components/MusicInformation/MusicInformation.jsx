0029846566b7473e8cf27b6ac7941e0940e142a2
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-12-23 21:56:03 +0100

    ✨💄💫 Musique info UI

--- src/components/MusicInformation/MusicInformation.jsx
+++ src/components/MusicInformation/MusicInformation.jsx
@@ -4,10 +4,15 @@ const MusicInformation= ({play}) => {
 
     return (
         <div className={s["Music__container"]}>
-            <img className={s["Music__cover"]} src="/asset/cover.webp" alt="" />
+            <div className={s["Music__cover"]}>
+                {play && <img src="/asset/MusicOn.svg" alt="" />}
+            </div>
+            {/* <img className={s["Music__cover"]} src="/asset/cover.webp" alt="" /> */}
             <div className={s["Music__info"]}>
-                <span className={s["Music__title"]}>Nightcall - Angèle, Kavinsky & Phoenix</span>
-                <span className={s["Music__title"]}>Nightcall - Angèle, Kavinsky & Phoenix</span>
+                    <span className={s["Music__title"]}>Nightcall - Angèle, Kavinsky & Phoenix</span>
+                    <span className={s["Music__title"]}>Nightcall - Angèle, Kavinsky & Phoenix</span>
+                    <span className={s["Music__title"]}>Nightcall - Angèle, Kavinsky & Phoenix</span>
+                    <span className={s["Music__title"]}>Nightcall - Angèle, Kavinsky & Phoenix</span>
             </div>
         </div>
     );
